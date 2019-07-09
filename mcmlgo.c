8f00d1d093c918ffbe41885218fb19a5f4c33657
Yuya Yamada <iris1212y@gmail.com>
2019-07-09 20:47:07 +0900

    Use MT for pseudorandom number generation.

--- mcmlgo.c
+++ mcmlgo.c
@@ -6,6 +6,7 @@
  ****/
 
 #include "mcml.h"
+#include "sfmt/SFMT.h"
 
 #define STANDARDTEST 0
   /* testing program using fixed rnd seed. */
@@ -30,41 +31,10 @@
 
 float ran3(int *idum)
 {
-  static int inext,inextp;
-  static long ma[56];
-  static int iff=0;
-  long mj,mk;
-  int i,ii,k;
-  
-  if (*idum < 0 || iff == 0) {
-    iff=1;
-    mj=MSEED-(*idum < 0 ? -*idum : *idum);
-    mj %= MBIG;
-    ma[55]=mj;
-    mk=1;
-    for (i=1;i<=54;i++) {
-      ii=(21*i) % 55;
-      ma[ii]=mk;
-      mk=mj-mk;
-      if (mk < MZ) mk += MBIG;
-      mj=ma[ii];
-    }
-    for (k=1;k<=4;k++)
-      for (i=1;i<=55;i++) {
-	ma[i] -= ma[1+(i+30) % 55];
-	if (ma[i] < MZ) ma[i] += MBIG;
-      }
-    inext=0;
-    inextp=31;
-    *idum=1;
-  }
-  if (++inext == 56) inext=1;
-  if (++inextp == 56) inextp=1;
-  mj=ma[inext]-ma[inextp];
-  if (mj < MZ) mj += MBIG;
-  ma[inext]=mj;
-  return mj*FAC;
-}
+  sfmt_t sfmt;
+  sfmt_init_gen_rand(&sfmt, *idum);
+  return sfmt_genrand_uint32(&sfmt);
+;}
 
 #undef MBIG
 #undef MSEED
